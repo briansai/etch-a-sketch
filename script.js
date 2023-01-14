405f1073934893303c0ae9b920dab1919efb555b
Brian Sai <brianwsai@gmail.com>
2023-01-13 19:08:45 -0800

    Created DOM manipulating class for less repetition

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ class Grid {
 }
 
 const board = new Grid(64);
+
+// DOM manipulating class
+class DOMManipulation {
+  constructor(el) {
+    this.el = el;
+  }
+
+  createEl() {
+    return document.createElement(this.el);
+  }
+
+  removeElByClass(className) {
+    const elToRemove = document.querySelector(`.${className}`);
+    elToRemove.remove();
+  }
+
+  appendElementTo(className) {
+    const parentEl = document.querySelector(`${className}`);
+    parentEl.append(this.createEl());
+  }
+}
