26909d8eea94be9948c9f8138c39d480998bd08c
Brian Sai <brianwsai@gmail.com>
2023-01-14 14:46:54 -0800

    Implemented initial grid on DOM

---
+++ modules.js
@@ -0,0 +1,53 @@
+// Sketch Grid
+
+class Grid {
+  constructor(squareNum) {
+    this.squareNum = squareNum;
+  }
+
+  createGrid() {
+    let arr = [];
+    const num = this.squareNum;
+    for (let x = 0; x < num; x++) {
+      arr.push([...Array(num).fill({ rgb: '255,255,255' })]);
+    }
+    return arr;
+  }
+
+  putGridOnBoard() {
+    for (let y = 1; y <= this.squareNum; y++) {
+      const newDiv = new DOMManipulation();
+      const row = newDiv.createEl('div', `row-${y}`);
+      newDiv.appendTo('.board', row);
+
+      for (let x = 1; x <= this.squareNum; x++) {
+        const newDiv = new DOMManipulation();
+        const square = newDiv.createEl('div', ` .square-${x}`);
+        square.textContent = x;
+        newDiv.appendTo(`.row-${y}`, square);
+      }
+    }
+  }
+}
+
+// DOM manipulating class
+
+class DOMManipulation {
+  createEl(el, className) {
+    const element = document.createElement(el);
+    element.className = className;
+    return element;
+  }
+
+  removeElByClass(className) {
+    const elToRemove = document.querySelector(`${className}`);
+    elToRemove.remove();
+  }
+
+  appendTo(parent, appendItem) {
+    const parentEl = document.querySelector(parent);
+    parentEl.append(appendItem);
+  }
+}
+
+export { DOMManipulation, Grid };
--- script.js
+++ script.js
@@ -1,39 +1,11 @@
-// Sketch Grid
+import { DOMManipulation, Grid } from './modules.js';
 
-class Grid {
-  constructor(squareNum) {
-    this.squareNum = squareNum;
-  }
+let newBoard = new Grid(64);
 
-  createGrid() {
-    let arr = [];
-    const num = this.squareNum;
-    for (let x = 0; x < num; x++) {
-      arr.push([...Array(num).fill({ rgb: '255,255,255' })]);
-    }
-    return arr;
-  }
-}
+// *Figure out what you did wrong here brian
+const domBoard = new DOMManipulation();
+const board = domBoard.createEl('div', 'board');
 
-const board = new Grid(64);
+const p = domBoard.appendTo('body', board);
 
-// DOM manipulating class
-class DOMManipulation {
-  constructor(el) {
-    this.el = el;
-  }
-
-  createEl() {
-    return document.createElement(this.el);
-  }
-
-  removeElByClass(className) {
-    const elToRemove = document.querySelector(`.${className}`);
-    elToRemove.remove();
-  }
-
-  appendElementTo(className) {
-    const parentEl = document.querySelector(`${className}`);
-    parentEl.append(this.createEl());
-  }
-}
+newBoard.putGridOnBoard();
