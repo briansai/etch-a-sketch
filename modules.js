e73fb35b0eec1cf024ddef05468b5f31342f09de
Brian Sai <brianwsai@gmail.com>
2023-01-18 18:39:22 -0800

    Calculated square size when grid size changes

--- modules.js
+++ modules.js
@@ -14,7 +14,10 @@ class Grid {
 
       for (let x = 1; x <= this.squareNum; x++) {
         const newDiv = new DOMManipulation();
-        const square = newDiv.createEl('div', ` square-${x}`);
+        const square = newDiv.createEl('div', `square-${x}`);
+
+        square.style.width = 500 / this.squareNum + 'px';
+        square.style.height = 500 / this.squareNum + 'px';
 
         square.addEventListener('mouseover', this.fillSquare);
 
@@ -28,6 +31,55 @@ class Grid {
     const classes = `.${path[1].className} .${path[0].className.trim()}`;
     const square = document.querySelector(classes);
     square.style.background = 'black';
+  }
+}
+
+// Slider
+
+class Slider {
+  constructor(val) {
+    this.val = val;
+  }
+
+  createSlider() {
+    const newManipulation = new DOMManipulation();
+
+    const slider = newManipulation.createEl('div');
+    slider.className = 'slider';
+
+    const sliderMain = newManipulation.createEl('input');
+    sliderMain.className = 'slider-main';
+    sliderMain.setAttribute('type', 'range');
+    sliderMain.setAttribute('min', 10);
+    sliderMain.setAttribute('max', 100);
+    sliderMain.setAttribute('value', 64);
+    sliderMain.oninput = this.changeValue;
+
+    const sliderText = newManipulation.createEl('div');
+    sliderText.className = 'slider-text';
+    sliderText.textContent = `${this.val} x ${this.val}`;
+
+    slider.append(sliderText);
+    slider.append(sliderMain);
+    newManipulation.appendTo('.main', slider);
+  }
+
+  changeValue(e) {
+    const newManipulation = new DOMManipulation();
+    const sliderText = newManipulation.selectEl('.slider-text');
+
+    this.val = e.target.value;
+    sliderText.textContent = `${this.val} x ${this.val}`;
+
+    const removeBoard = newManipulation.selectEl('.board');
+    removeBoard.remove();
+
+    const newBoard = new Grid(this.val);
+    const board = newManipulation.createEl('div', 'board');
+
+    newManipulation.appendTo('.main', board);
+
+    newBoard.putGridOnBoard();
   }
 }
 
@@ -55,4 +107,4 @@ class DOMManipulation {
   }
 }
 
-export { DOMManipulation, Grid };
+export { DOMManipulation, Grid, Slider };
--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-import { DOMManipulation, Grid } from './modules.js';
+import { DOMManipulation, Grid, Slider } from './modules.js';
 
-const newBoard = new Grid(64);
+// Manipulate DOM
 const newManipulation = new DOMManipulation();
-const board = newManipulation.createEl('div', 'board');
+
+// Create main
 const main = newManipulation.createEl('div', 'main');
 newManipulation.appendTo('body', main);
+
+// Create slider
+const newSlider = new Slider(64);
+newSlider.createSlider();
+
+// Create new grid
+const newBoard = new Grid(64);
+const board = newManipulation.createEl('div', 'board');
+
 newManipulation.appendTo('.main', board);
 
 newBoard.putGridOnBoard();
